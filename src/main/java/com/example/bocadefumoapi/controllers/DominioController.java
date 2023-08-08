d927eb9ed3c3f019bfd52d00cd7dfc6ccc9e18e2
Lucas <lcs999999999@hotmail.com>
2023-08-08 18:36:53 -0300

    Adicionando CRUD de dominios para tipagem de entidades e relações

--- src/main/java/com/example/bocadefumoapi/controllers/DominioController.java
+++ src/main/java/com/example/bocadefumoapi/controllers/DominioController.java
@@ -8,9 +8,11 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class DominioController {
@@ -23,5 +25,29 @@ public class DominioController {
         var dominioModel = new DominioModel();
         BeanUtils.copyProperties(dominioBody, dominioModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(dominioRepo.save(dominioModel));
+    }
+
+    @GetMapping("/dominios")
+    public ResponseEntity<List<DominioModel>> getAllDominio(){
+        return ResponseEntity.status(HttpStatus.OK).body(dominioRepo.findAll());
+    }
+
+    @GetMapping("/dominios/{guidDominio}")
+    public ResponseEntity<Object> getOneDominio(@PathVariable(value = "guidDominio") UUID guidDominio){
+        Optional<DominioModel> dominio = dominioRepo.findById(guidDominio);
+        if (dominio.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Dominio não encontrado");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(dominio.get());
+    }
+
+    @DeleteMapping("/dominios/{guidDominio}")
+    public ResponseEntity<Object> deleteDominio(@PathVariable(value = "guidDominio") UUID guidDominio){
+        Optional<DominioModel> dominio = dominioRepo.findById(guidDominio);
+
+        if (dominio.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Dominio não encontrado.");
+        }
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Doninio deletado.");
     }
 }
